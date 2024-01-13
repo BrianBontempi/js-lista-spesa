48a1c207ed4658b3888c0e76e37313a7c026fb79
BrianBontempi <brian.bont54@gmail.com>
2024-01-13 15:18:51 +0100

    add ciclo while

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ const listaSpesa = ["Pane", "Latte", "Uova", "Frutta", "Verdura"];
 for (let i = 0; i < listaSpesa.length; i++) {
     console.log(listaSpesa[i]);
 }
+
+
+// Inizializziamo il contatore per il ciclo while
+const shoppingList = ["Biscotti", "Lasagne", "Pizza", "Surgelati"]
+let counter = 0;
+
+// Ciclo while per stampare gli elementi della lista
+while (counter < shoppingList.length) {
+    console.log(shoppingList[counter]);
+    counter++;
+}
