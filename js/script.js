4612b0395b0a5d595f8e92bd3dbbb7c132dc2cd6
BrianBontempi <brian.bont54@gmail.com>
2024-01-13 15:15:07 +0100

    add ciclo for

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("Ciao")
+// Lista della spesa
+const listaSpesa = ["Pane", "Latte", "Uova", "Frutta", "Verdura"];
+
+// Ciclo for per stampare gli elementi della lista
+for (let i = 0; i < listaSpesa.length; i++) {
+    console.log(listaSpesa[i]);
+}
